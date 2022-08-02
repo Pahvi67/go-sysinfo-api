ba83c0c531e25a031062cbcd7ce05403f8f9b68d
Pahvi67 <jonivirta94@gmail.com>
2022-08-02 19:25:23 +0300

    Read .env to session if not inside docker

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# get env variables if not inside docker
+if ! [ -f /.dockerenv ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
 if [ "$ENVIRONMENT" = 'development' ]; then
     echo "Starting in dev env..."
     exec go run status-api.go
